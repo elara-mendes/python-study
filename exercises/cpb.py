8e848da35ff4c15bedb796b8869abf6049f2e3d8
elara-mendes <elaramendes@yahoo.com>
2025-01-25 22:34:44 -0300

    Upload exercises

---
+++ exercises/cpb.py
@@ -0,0 +1,10 @@
+n1 = 6.3
+n2 = 4.5
+
+resultado_parcial = (n1 * 1 + n2 * 2) / (1 + 2) # Só tem duas notas
+
+n3 = 6.5 * 6 # Média que desejamos pra passar 6.5 vezes 6 (1+2+3)
+
+md = (n3-(n1*1 + n2*2))/3 # 39 - 15.3 = 23.7 / 3 = 7.9 !!!
+print(round(resultado_parcial, 2))
+print(f"This {round(md, 2)}")
---
+++ exercises/test_sort.py
@@ -0,0 +1,5 @@
+alphabet = ["z", "b", "d", "c"]
+alphabet.sort() # Reverse parameter always False
+
+for letter in alphabet:
+    print(letter)
