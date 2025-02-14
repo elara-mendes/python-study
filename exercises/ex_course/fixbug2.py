0cd26a17059b45500796d4a3fe5cdd4af5fb0554
elara-mendes <elaramendes@yahoo.com>
2025-02-14 00:44:25 -0300

    Upload exercises

--- exercises/ex_course/fixbug2.py
+++ exercises/ex_course/fixbug2.py
@@ -2,13 +2,21 @@
 # numbers = [int(number) for number in numbers]
 # print(numbers)
 
-def speed(distance, time):
-    return distance / time
+# def speed(distance, time):
+#     return distance / time
+#
+# print(speed(200, 4))
+#
+#
+# def speed(distance, time):
+#     return distance / time
+#
+# print(speed(300, 5))
 
-print(speed(200, 4))
+def calculate_time(h, g=9.80665):
+    t = (2 * h / g) ** 0.5
+    return t
 
 
-def speed(distance, time):
-    return distance / time
-
-print(speed(300, 5))
+time = calculate_time(100) # Solved
+print(time)
--- exercises/feet_inches.py
+++ exercises/feet_inches.py
@@ -1,12 +1,19 @@
 feet_inches = input("What's the feet and inches? ")
 
-def convert_to_meters(meters_value):
+
+def parse(feet_inches):
     feet_value = float(feet_inches[0])
     inches_value = float(feet_inches[2:3])
+    return {"feet": feet_value, "inches": inches_value}
+
+
+def convert_to_meters(feet_value, inches_value):
     meters_value = (feet_value * 0.3048) + (inches_value * 0.0254)
     return f"The feet is {feet_value}, the inches is {inches_value} and the result in meters is {round(meters_value, 2)}.", meters_value
 
-result = convert_to_meters(feet_inches)
+
+parsed = parse(feet_inches)
+result = convert_to_meters(parsed["feet"], parsed["inches"])
 
 if result[1] > 0:
     print(result[0])
