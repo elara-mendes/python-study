37ffcb15ae857d7294125a22bbcdb690f054d97f
elara-mendes <elaramendes@yahoo.com>
2025-02-14 18:00:40 -0300

    Upload exercises

---
+++ exercises/exfn/main.py
@@ -0,0 +1,10 @@
+from functions import count
+
+# nr_of_periods = functions.count("Trees are good. Grass is green.")
+# print(nr_of_periods)
+
+# nr_of_periods = functions.count("Trees are good. Grass is green.")
+# print(nr_of_periods)
+
+nr_of_periods = count("Trees are good. Grass is green.")
+print(nr_of_periods)
