23f4d3742d22b0e87900f8ca81038449bb9ac07c
elara-mendes <elaramendes@yahoo.com>
2025-02-19 22:23:39 -0300

    Upload exercises

--- exercises/feet_inches_gui.py
+++ exercises/feet_inches_gui.py
@@ -2,17 +2,24 @@ import FreeSimpleGUI as sg
 
 def convert_to_meters(feet_value, inches_value):
     meters_value = (feet_value * 0.3048) + (inches_value * 0.0254)
-    return f"The feet is {feet_value}, the inches is {inches_value} and the result in meters is {round(meters_value, 2)}."
+    return f"{round(meters_value, 2)}"
 
 
 text = sg.Text("Convert feet/inches to m!")
 feet_label = sg.Input(key="feet"),
 inches_label = sg.Input(key="inches"),
 button = sg.Button('Convert')
+text_m = sg.Text("")
 
 # Create the window
-window = sg.Window('Convertor', layout=[[text], [feet_label], [inches_label], [button]])
+window = sg.Window('Convertor', layout=[[text], [feet_label], [inches_label], [button, text_m]])
 
-events, values = window.read()
+while True:
+    events, values = window.read()
 
-sg.popup(convert_to_meters(float(values["feet"]), float(values["inches"])))
+    if events == "Convert":
+        text_m.update((convert_to_meters(float(values["feet"]), float(values["inches"]))))
+        # sg.popup(convert_to_meters(float(values["feet"]), float(values["inches"])))
+
+    if events == sg.WIN_CLOSED or events == 'Exit':
+        break
--- exercises/gui_zip.py
+++ exercises/gui_zip.py
@@ -7,20 +7,35 @@ input_label = sg.Input(key="file"), sg.FilesBrowse()
 output_label = sg.Input(key="output"), sg.FolderBrowse()
 button = sg.Button('Compress')
 
+
 # Create the window
-window = sg.Window('Compressator', layout=[[text], [input_label], [output_label], [button]])
+window = sg.Window('Compressator',
+                   layout=[[text], [input_label],
+                           [output_label], [button]])
 
-events, values = window.read()
+def make_archive(output_path):
+    with zp.ZipFile(output_path, "w") as zipf:
+        for file in select_file:
+            zipf.write(file, os.path.basename(file))
 
-output = values["output"]
-file_name = "my_file.zip"
-file_input = values["file"]
-select_file = file_input.split(";")
-output_path = os.path.join(output, file_name)
-with zp.ZipFile(output_path, "w") as zipf:
-    for file in select_file:
-        zipf.write(file, os.path.basename(file))
+while True:
+    events, values = window.read()
+    filename = ""
+    if events == "Compress":
+        filename = sg.popup_get_text("Write the name of the file:")
+    output = values["output"]
+    file_name = filename + ".zip"
+    file_input = values["file"]
+    select_file = file_input.split(";")
+    output_path = os.path.join(output, file_name)
+    make_archive(output_path)
+    sg.popup("The file was compressed with success!")
 
-print(f"The file was compressed with success!")
+    if events == sg.WIN_CLOSED or events == 'Exit':
+        break
 
 window.close()
+
+"""
+My teacher is using pathlib
+"""
